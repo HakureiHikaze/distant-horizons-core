52b8a91dc5a69071cfeb56ffcbc88de75bd0f823
James Seibel <jeseibel@gondtc.com>
2026-05-16 10:16:10 -0500

    fix cleanup threadpool preventing JVM shutdown

--- core/src/main/java/com/seibel/distanthorizons/core/util/threading/ThreadPoolUtil.java
+++ core/src/main/java/com/seibel/distanthorizons/core/util/threading/ThreadPoolUtil.java
@@ -60,7 +60,7 @@ public class ThreadPoolUtil
 	public static PriorityTaskPicker.Executor getWorldGenExecutor() { return worldGenThreadPool; }
 	
 	public static final String CLEANUP_THREAD_NAME = "Cleanup";
-	private static final ThreadPoolExecutor cleanupThreadPool = ThreadUtil.makeSingleThreadPool(CLEANUP_THREAD_NAME);
+	private static final ThreadPoolExecutor cleanupThreadPool = ThreadUtil.makeSingleDaemonThreadPool(CLEANUP_THREAD_NAME);
 	/** not null since cleanup always needs to be run even when DH has been shut down */
 	@NotNull
 	public static ThreadPoolExecutor getCleanupExecutor() { return cleanupThreadPool; }
