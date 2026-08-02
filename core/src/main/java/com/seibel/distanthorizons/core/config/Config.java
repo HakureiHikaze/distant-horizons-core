c879573dd5e490cd97fdd55b5b2593186804fd1b
James Seibel <jeseibel@gondtc.com>
2026-08-02 08:42:16 -0500

    add grass to texture side blocks

--- core/src/main/java/com/seibel/distanthorizons/core/config/Config.java
+++ core/src/main/java/com/seibel/distanthorizons/core/config/Config.java
@@ -707,7 +707,8 @@ public class Config
 						.set("grass_block," +
 							"mycelium," +
 							"nylium," +
-							"dirt_path" +
+							"dirt_path," +
+							"grass" +
 							"")
 						.setAppearance(EConfigEntryAppearance.ALL)
 						.comment(""
